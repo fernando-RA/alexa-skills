918abcecf9489b372e810abffe1fe72756e3a67e
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-08-20 14:46:58 -0300

    Refactoring files & installing express

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from './Footer';
-import CitationsComponent from './CitationsComponent';
+import Footer from './components/Footer';
+import CitationsComponent from './components/CitationsComponent';
 // import './style.css';
 
 function App() {
