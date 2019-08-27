633f6361583c5b63b605be1c7f59e7cc94997cdd
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-08-27 06:46:32 -0300

    API is working properly with proxy to client

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+    const response = await fetch('http://localhost:9000/testAPI');
+    const body = await response.text();
 
     if (response.status !== 200) throw Error(body.message);
 
