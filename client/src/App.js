7eb807cce07ab613d5792973b04910270f2aaaca
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-09-04 20:37:30 -0300

    prototyping version zero of frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,36 +7,17 @@ import CitationComponent from './components/CitationsComponent'
 import './style.css';
 
 class App extends Component {
-  state = {
-    response: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('http://localhost:9000/quote');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
   render() {
     return (
       <div className="App">
       <Header />
       <CitationComponent />
-        <div>
+        {/* <div>
           <h1>Quote Text: </h1>
           <p>{this.state.response.quoteText}</p>
           <h2>Author: </h2>
           <p>{this.state.response.quoteAuthor}</p>
-        </div>
+        </div> */}
         <Footer/>
       </div>
     );
--- client/src/components/CitationsComponent.js
+++ client/src/components/CitationsComponent.js
@@ -1,76 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-async function callAPI(){
-	const db_response = await fetch('/api');
-	console.log(db_response);
-	
-	const db_json = await db_response.json();
-	
-	console.log("db:", db_json);
-	return db_json;
-}
+class CitationsComponent extends Component {
+	state = {
+		response: ''
+	};
 
-export default function CitationsComponent() {
-	
-	callAPI();
+	componentDidMount() {
+		this.callApi()
+			.then(res => this.setState({ response: res }))
+			.catch(err => console.log(err));
+	}
 
-	// TODO -> abstraction
-	return (
-		<section className="container sobre">
+	callApi = async () => {
+		const response = await fetch('http://localhost:9000/quote');
+		const body = await response.json();
+
+		if (response.status !== 200) throw Error(body.message);
+
+		return body;
+	};
+
+	render() {
+		const quoteText = this.state.response.quoteText;
+		const numChars = quoteText === undefined ? 0 : Array.from(quoteText).lenght;
+		console.log(numChars);
 		
-			<h1 className="text-center"> Sobre Programação</h1>
-			<p>
-				Este site exibe citações aletórias do campo da programação usando API Storm e Grimmstudios. Se você
-				quiser, você pode tweetar as citações.
-			</p>
+		return (
+			<section className="container sobre">
+				<h1 className="text-center"> Sobre Programação</h1>
+				<p>
+					Este site exibe citações aletórias <br /> Se você quiser, você pode tweetar as citações.
+				</p>
 
-			<div className="load text-center">
-				<i className="fa fa-spinner fa-spin fa-3x" />
-			</div>
-
-			<blockquote className="container ">
-				<div className=" well ">
-					<q className="citacao-texto" />
+				<div className="load text-center">
+					<i className="fa fa-spinner fa-spin fa-3x" />
 				</div>
-				<div>
-					<div>
-						<strong>&mdash;</strong> <span className="autor" />
+
+				<blockquote className="container ">
+					<div className=" well ">
+						<q className="citacao-texto">{this.state.response.quoteText}</q>
 					</div>
-				</div>
+					<div>
+						<div>
+							<strong>&mdash;</strong>
+							<span className="autor">{this.state.response.quoteAuthor}</span>
+						</div>
+					</div>
 
-				<div className="contentTraducao">
+					{/* <div className="contentTraducao">
 					<p>
 						<span className="txtTraduzido" />
 						&#8212;<span className="autorApi" />
 					</p>
-				</div>
-			</blockquote>
+				</div> */}
+				</blockquote>
 
-			<select className="lang-select  pull-right">
-				<option value="pt">Português</option>
-				<option value="es">Espanhol</option>
-				<option value="en">Inglês</option>
-				<option value="fr">Francês</option>
-			</select>
+				<select className="lang-select  pull-right">
+					<option value="pt">Português</option>
+					<option value="es">Espanhol</option>
+					<option value="en">Inglês</option>
+					<option value="fr">Francês</option>
+				</select>
 
-			<div className="wrapperBtns">
-				<button className="btn btn-primary" type="button" id="btnCitacao" >
-					<i className="fa fa-refresh  fa-4x" />
-				</button>
-				<a
-					className="twitter-share-button"
-					href="https://twitter.com/intent/tweet"
-					data-size="large"
-				>
-					<button className="btn btn-primary " type="button" id="btTw">
-						{' '}
-						<i className="fa fa-twitter fa-4x" />{' '}
+				<div className="wrapperBtns">
+					<button className="btn btn-primary" type="button" id="btnCitacao">
+						<i className="fa fa-refresh  fa-4x" />
 					</button>
-				</a>
-				<div>
-					caracteres:<span id="numChars" />
+					<a className="twitter-share-button" href="https://twitter.com/intent/tweet" data-size="large">
+						<button className="btn btn-primary " type="button" id="btTw">
+							{' '}
+							<i className="fa fa-twitter fa-4x" />{' '}
+						</button>
+					</a>
+					<div>
+						caracteres:<span id="numChars">{numChars}</span>
+					</div>
 				</div>
-			</div>
-		</section>
-	);
+			</section>
+		);
+	}
 }
+
+export default CitationsComponent;
